7c699924e699a8babb42d1a821559733cc1b4f25
ramitarora <ramit91@gmail.com>
2014-02-11 20:57:11 -0600

    Initial Commit from local git

---
+++ CS414MP1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CS414MP1
+//
+//  Created by Ramit Arora on 2/11/14.
+//  Copyright (c) 2014 White Water. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
