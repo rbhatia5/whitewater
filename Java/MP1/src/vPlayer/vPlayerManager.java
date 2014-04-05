0945947d2ed73b13269168157f40f029dbfe5fde
Zain114 <qazi2@illinois.edu>
2014-04-04 22:03:07 -0500

    server code

--- Java/MP1/src/vPlayer/vPlayerManager.java
+++ Java/MP1/src/vPlayer/vPlayerManager.java
@@ -9,14 +9,18 @@ import javax.swing.*;
 
 import org.gstreamer.*;
 import org.gstreamer.swing.*;
+import org.gstreamer.elements.*;
+import org.gstreamer.elements.good.RTPBin;
 
 public class vPlayerManager {
+	
+	private static Pipeline pipe;
 	
 	public static void main(String[] args)
 	{
 		//initialize GStreamer
 		args = Gst.init("Simple Pipeline", args);
-		
+		/*
 		//set startup mode
 		PlayerData.mode = PlayerData.Mode.PLAYER;
 		PlayerData.vidEnc = PlayerData.VideoEncoding.MJPEG;
@@ -68,11 +72,90 @@ public class vPlayerManager {
 	            });	
 	            timer.start();
 	            */
+		/*
 	        } 
 	    });
+		*/
 		
-		//PlayerData.pipe.setState(State.PLAYING);
-		//Gst.main();
-		//
+		////////////////////////////////////////////////////SERVER//////////////////////////////////////////////////////////
+		/*
+		pipe = new Pipeline("server-pipeline");
+		
+		Element source = ElementFactory.make("v4l2src", "webcam");
+		Element encoder = ElementFactory.make("ffenc_h263", "encoder");
+		Element pay = ElementFactory.make("rtph263pay", "pay");
+		RTPBin rtpBin = (RTPBin)ElementFactory.make("gstrtpbin", "rtp-bin"); 
+		Element udpSink = ElementFactory.make("udpsink", "udpsink");
+		
+		if(source == null || encoder == null || pay == null || rtpBin == null || udpSink == null)
+			System.err.println("Could not create all elements");
+		
+		pipe.addMany(source, encoder, pay, rtpBin, udpSink);
+		
+		Element.linkMany(source, encoder, pay);
+		
+		udpSink.set("host", "127.0.0.1");
+		udpSink.set("port", "5001");
+		
+		rtpBin.connect(new Element.PAD_ADDED() {
+			public void padAdded(Element source, Pad newPad) {
+				System.out.printf("New pad %s added to %s\n", newPad.getName(), source.getName());
+				Pad udpSinkPad = pipe.getElementByName("udpsink").getStaticPad("sink");
+				System.out.println(udpSinkPad.getName());
+				newPad.link(udpSinkPad);
+			}
+		});
+		
+		Pad send_rtp_sink_0 = rtpBin.getRequestPad("send_rtp_sink_0");
+		Pad paySrcPad = pay.getStaticPad("src");
+		if(send_rtp_sink_0 == null || paySrcPad == null)
+			System.err.println("Could not create rtpbin.send_rtp_sink_0 or pay.src pad");
+		paySrcPad.link(send_rtp_sink_0);
+		*/
+		////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		
+		
+		
+		////////////////////////////////////////////////////CLIENT//////////////////////////////////////////////////////////
+		
+		pipe = new Pipeline("client-pipeline");
+		
+		Element udpSrc = ElementFactory.make("udpsrc", "udp-src");
+		System.out.println("1 " + udpSrc);
+		RTPBin rtpBin = (RTPBin)ElementFactory.make("gstrtpbin", "rtp-bin");
+		System.out.println("2 " + rtpBin);
+		Element depay = ElementFactory.make("rtph263depay", "depay");
+		System.out.println("3 " + depay);
+		Element decoder = ElementFactory.make("ffdec_h263", "decoder");
+		System.out.println("4 " + decoder);
+		Element sink = ElementFactory.make("xvimagesink", "sink");
+		System.out.println("5 " + sink);
+		
+		Caps udpCaps = Caps.fromString("application/x-rtp,encoding-name=(string)H263,media=(string)video,clock-rate=(int)90000,payload=(int)96");
+		System.out.println("6 " + udpCaps.toString());
+		udpSrc.setCaps(udpCaps);
+		udpSrc.set("port", "5001");
+		
+		rtpBin.connect(new Element.PAD_ADDED() {
+			public void padAdded(Element source, Pad newPad) {
+				System.out.printf("New pad %s added to %s\n", newPad.toString(), source.toString());
+			}
+		});
+		
+		pipe.addMany(udpSrc, rtpBin, depay, decoder, sink);
+		
+		Pad recv_rtp_sink_0 = rtpBin.getRequestPad("recv_rtp_sink_0");
+		System.out.println("7 " + recv_rtp_sink_0);
+		Pad udpSrcPad = udpSrc.getStaticPad("src");
+		System.out.println("8 " + udpSrcPad);
+		//PadLinkReturn ret = udpSrcPad.link(recv_rtp_sink_0);
+		//System.out.println("9 " + ret.toString());
+		boolean link = Element.linkMany(depay, decoder, sink);
+		System.out.println("10 " + link);
+		
+		////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		
+		pipe.setState(State.PLAYING);
+		Gst.main();
 	}
 }
